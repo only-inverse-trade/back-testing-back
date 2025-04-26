d5904389f24f15385d6e53b47c9e4e26669d3fa5
llvnpys <naysung03@hanyang.ac.kr>
2025-04-26 15:22:26 +0900

    first commit

---
+++ src/main/java/com/ost/back_testing_back/BackTestingBackApplication.java
@@ -0,0 +1,13 @@
+package com.ost.back_testing_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTestingBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTestingBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ost/back_testing_back/BackTestingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ost.back_testing_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTestingBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e8a31b06241b336fdf23e1e8bf3c0d922c2fc635
llvnpys <naysung03@hanyang.ac.kr>
2025-04-27 17:14:05 +0900

    feat(backtest-repository / service): 백테스트 레포, DTO, 서비스 로직 추가
    백테스트 repo, dto, service 추가.
    webclient 추가

---
+++ src/main/java/com/ost/back_testing_back/service/BacktestService.java
@@ -0,0 +1,64 @@
+package com.ost.back_testing_back.service;
+
+import com.ost.back_testing_back.dto.BacktestDto;
+import com.ost.back_testing_back.entity.BacktestReq;
+import com.ost.back_testing_back.entity.BacktestRes;
+import com.ost.back_testing_back.repository.BacktestReqRepository;
+import com.ost.back_testing_back.repository.BacktestResRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class BacktestService {
+
+    private final BacktestReqRepository backtestReqRepository;
+    private final BacktestResRepository backtestResRepository;
+    private final WebClient webClient;
+
+    public void processBacktest(BacktestDto.CreateBacktestRequest requestDto) {
+        try {
+            BacktestDto.CreateBacktestResponse success = runBacktestLogic(requestDto);
+//            String status = success ? "SUCCESS" : "FAILED";
+            String status = "SUCCESS";
+            BacktestReq req = saveBacktestRequest(requestDto, status);
+            saveBacktestResult(req, success.totalReturn());
+
+        } catch (Exception e) {
+            // 실패 시 예외 처리
+        }
+    }
+
+    private BacktestReq saveBacktestRequest(BacktestDto.CreateBacktestRequest requestDto, String status) {
+        return backtestReqRepository.save(
+                BacktestReq.builder()
+                        .ticker(requestDto.ticker())
+                        .startDate(LocalDate.parse(requestDto.startDate()))
+                        .endDate(LocalDate.parse(requestDto.endDate()))
+                        .strategy(requestDto.strategy())
+                        .status(status)
+                        .build()
+        );
+    }
+
+    private void saveBacktestResult(BacktestReq req, double totalReturn) {
+        backtestResRepository.save(
+                BacktestRes.builder()
+                        .requestId(req)
+                        .totalReturn(totalReturn)
+                        .build()
+        );
+    }
+
+    private BacktestDto.CreateBacktestResponse runBacktestLogic(BacktestDto.CreateBacktestRequest requestDto) {
+        return webClient.post()
+                .uri("/api/backtest") // ML 서버 API 엔드포인트
+                .bodyValue(requestDto)
+                .retrieve()
+                .bodyToMono(BacktestDto.CreateBacktestResponse.class)
+                .block(); // 결과 기다림
+    }
+}
